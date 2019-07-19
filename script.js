ca771d0f601f71de652568340db264a745157927
Beatriz Pavan de Lima <bia.pavan1904@gmail.com>
2019-07-19 17:43:01 -0300

    Realização da atividade número 1

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-let dia1 = "Domingo";
-let dia2 = "Segunda-feira";
-let dia3 = "Terça-feira";
-let dia4 = "Quarta-feira";
-let dia5 = "Quinta-feira";
-let dia6 = "Sexta-feira";
-let dia7 = "Sábado";
+let idade = Number(prompt("Digite sua idade"));
 
-let numero = Number(prompt("Digite um número de 1 a 7"));
+if (idade >= 25 && idade <= 60) {
+    let salario = Number(prompt("Digite seu salário"));
+    if (salario >= 2000) {
+        let emprestimo = Number(prompt("Digite o valor desejado para empréstimo"));
+        if (emprestimo < 10 * salario) {
+            console.log("Emprestimo Aprovado");
 
-if(numero == 1){
-    console.log(`O dia ${numero} é ${dia1}`);
+        } else {
+            console.log("Empréstimo negado por exeder valor");
+        }
+
+    } else {
+        console.log("Operação reprovada por salário");
+
+    }
+} else {
+    console.log("Recusado porque não tem idade pra isso")
 }
 
-else if(numero == 2){
-    console.log(`O dia ${numero} é ${dia2}`);
-}
-else if(numero == 3){
-    console.log(`O dia ${numero} é ${dia3}`);
-}
-else if(numero == 4){
-    console.log(`O dia ${numero} é ${dia4}`);
-}
-else if(numero == 5){
-    console.log(`O dia ${numero} é ${dia5}`);
-}
-else if(numero == 6){
-    console.log(`O dia ${numero} é ${dia6}`);
-}
-else if(numero ==7){
-    console.log(`O dia ${numero} é ${dia7}`);
-}
+
+
+
+
+
+
+
+
+
